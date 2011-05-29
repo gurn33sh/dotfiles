5b75d2304766466be85b1266825f4e61dab8da3d
Will Leinweber <will@bitfission.com>
2011-05-28 18:03:20 -0700

    mark

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -11,4 +11,4 @@ alias gd="git diff"
 alias git="nocorrect git"
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 
-alias done='growlnotify -s -m done && say done'
+alias ddone='growlnotify -s -m done && say done'
